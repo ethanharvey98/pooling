29776c48607aeece160ab48f86773d43301587a7
loevlie <loevliedenny@gmail.com>
2026-01-06 12:00:18 -0500

    Adding rsna preprocessing script

--- src/preprocess_rsna.py
+++ src/preprocess_rsna.py
@@ -6,14 +6,12 @@ import pydicom
 # Importing our custom module(s)
 import ct
 
-# python ../src/preprocess_rsna.py --csv_path='/home/denny-loevlie/RSNA_Investigation/rsna_ich_subset_1149_complete.csv' --dicom_dir='/media/M2SSD/gen_models_data/stage_2_train' --numpy_dir='/media/M2SSD/gen_models_data/RSNA_ICH_numpy' --start=0 --stop=100
+# python ../src/preprocess_rsna.py --csv_path='/home/denny-loevlie/RSNA_Investigation/rsna_ich_subset_1149_complete.csv' --dicom_dir='/media/M2SSD/gen_models_data/stage_2_train' --numpy_dir='/media/M2SSD/gen_models_data/RSNA_ICH_numpy'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="preprocess_rsna.py")
     parser.add_argument("--csv_path", help="Path to CSV with scan/slice IDs", type=str)
     parser.add_argument("--dicom_dir", help="Directory containing DICOM files", type=str)
     parser.add_argument("--numpy_dir", help="Directory to save numpy dataset", type=str)
-    parser.add_argument("--start", default=0, help="Start index (default: 0)", type=int)
-    parser.add_argument("--stop", default=10, help="End index (default: 10)", type=int)
     args = parser.parse_args()
 
     os.makedirs(args.numpy_dir, exist_ok=True)
@@ -21,7 +19,7 @@ if __name__ == "__main__":
     df = pd.read_csv(args.csv_path)
     scan_ids = df['scan_id'].unique()
 
-    for scan_id in scan_ids[args.start:args.stop]:
+    for scan_id in scan_ids:
 
         slice_info = df[df['scan_id'] == scan_id].sort_values('slice_order')
         images = []
