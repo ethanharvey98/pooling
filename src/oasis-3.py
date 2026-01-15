7b705ddeba7965a0d68d451da65487365a3bb29c
Ethan Harvey <ethan.harvey@tufts.edu>
2026-01-15 13:46:10 -0500

    Added new early stopping for saving models

--- src/oasis-3.py
+++ src/oasis-3.py
@@ -80,5 +80,6 @@ if __name__=='__main__':
         
         model_history_df.to_csv(f'{args.experiments_dir}/{args.model_name}.csv')
     
-        if args.save and epoch == model_history_df.val_auroc.idxmax():
-            torch.save(model.state_dict(), f'{args.experiments_dir}/{args.model_name}.pt')
+        val_auroc_series = model_history_df[model_history_df.train_auroc > model_history_df.val_auroc].val_auroc
+        if args.save and epoch == (val_auroc_series.idxmax() if not val_auroc_series.empty else None):
+            torch.save(model.state_dict(), f"{args.experiments_dir}/{args.model_name}.pt")
