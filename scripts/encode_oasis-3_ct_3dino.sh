b5eb787227943e144861ff6e315e56cdc09d92e8
Dennis Loevlie <dennisloevlie@Denniss-MacBook-Pro.local>
2026-03-13 12:34:08 -0400

    Add OASIS-3 CT encoding and training scripts for 3DINO

---
+++ scripts/encode_oasis-3_ct_3dino.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --array=0-2
+#SBATCH --error=/cluster/tufts/hugheslab/dloevl01/slurmlog/err/log_%j.err
+#SBATCH --gres=gpu:rtx_a6000:1
+#SBATCH --mem=16g
+#SBATCH --ntasks=4
+#SBATCH --output=/cluster/tufts/hugheslab/dloevl01/slurmlog/out/log_%j.out
+#SBATCH --partition=hugheslab
+#SBATCH --time=24:00:00
+
+source ~/.bashrc
+conda activate /cluster/tufts/hugheslab/dloevl01/DINO3_Experiments/pooling/envs/dino3
+
+seeds=(1001 2001 3001)
+seed=${seeds[$SLURM_ARRAY_TASK_ID]}
+
+# Set this to the cached weights path (find with: find ~/.cache/huggingface -name "3dino_vit_weights.pth")
+WEIGHTS="/cluster/tufts/hugheslab/dloevl01/DINO3_Experiments/pooling/envs/dino3/3dino_vit_weights.pth"
+
+python ../src/encode_oasis-3_3dino.py \
+    --encoded_dir="/cluster/tufts/hugheslab/eharve06/encoded_OASIS-3_CT/3DINO_ViT_concat/seed=${seed}" \
+    --numpy_dir='/cluster/tufts/hugheslab/datasets/OASIS-3_CT_numpy' \
+    --pretrained_weights="${WEIGHTS}" \
+    --seed=${seed} \
+    --n_last_blocks=4 \
+    --avgpool
+
+conda deactivate
